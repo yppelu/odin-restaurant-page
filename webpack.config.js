8ae6eb1171cbca5e34234bc98ced5c3dc2cd52cc
yppelu <yppelu@gmail.com>
2023-10-01 20:35:19 +0700

    Add basic styles

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
     clean: true,
-    assetModuleFilename: '[name][ext]',
+    assetModuleFilename: './assets/[name][ext]',
   },
 }
